37c105cc56ffc7946ba295199a46c95ce0e9bf77
Ratan <ratanrathod870@gmail.com>
2024-09-01 16:18:01 +0530

    part2 responsive code

--- components/Page1.tsx
+++ components/Page1.tsx
@@ -21,7 +21,7 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({ words, interval = 1000 }) =
   }, [words, interval]);
 
   return (
-    <div className=" overflow-hidden inline-block absolute h-[1.3em] w-[5.65em] ml-[1.5vw]  ">
+    <div className=" overflow-hidden inline-block lg:absolute md:absolute lg:h-[1.3em] w-full sm:w-[5.65em] sm:ml-[1.5vw]  lg:ml-6 ml-2 ">
       <AnimatePresence mode="wait">
         <motion.div
           key={currentWordIndex}
@@ -49,12 +49,17 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({ words, interval = 1000 }) =
 
 const Page1: React.FC = () => {
   return (    
-    <div className='page-part-1 w-full h-[32vw] p-16 pt-20 flex font-[Satoshi] '> 
-      <div className="content flex flex-col h-full mt-24 text-[6vw] tracking-[0.1vw] leading-[6vw] font-semibold relative">
-        <h1 className='text-white'>Design studio</h1>
-        <h1 className='text-white'>for timeless <AnimatedText words={['strategy', 'packaging', 'motion', 'naming', 'branding']} /></h1> 
-      </div>
+    <div className='page-part-1 w-full h-[44vw] lg:h-[27vw] sm:h-[39vw] md:h-[45vw] p-4  lg:p-14 sm:p-4 md:p-14 pt-10 sm:pt-10 flex font-[Satoshi]'>
+    <div className="content flex flex-col h-full mt-12 sm:mt-24 text-2xl sm:text-4xl md:text-5xl lg:text-[6vw] tracking-tigher sm:tracking-tighter leading-tight sm:leading-[1.1] md:leading-[6vw] font-semibold relative">
+      <h1 className='text-white mb-2 sm:mb-0'>Design studio</h1>
+      <h1 className='text-white flex '>
+        for timeless{' '}
+        <span className="block sm:inline relative h-[1.3em]">
+          <AnimatedText words={['strategy', 'packaging', 'motion', 'naming', 'branding']} />
+        </span>
+      </h1>
     </div>
+  </div>
 
   );
 };
